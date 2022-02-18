19d688e9bdab79054c789d96f1714f872c1bb409
Arian <adilsonarian@outlook.com>
2022-02-18 10:19:26 -0600

    Interfaz principal y funcionalidad del boton encolar en proceso

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,92 +29,141 @@ namespace Ejercicio_1_Desafio_Practico_1
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnEncolar = new System.Windows.Forms.Button();
+            this.btnDesencolar = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDown)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btnEncolar
             // 
-            this.button1.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(281, 11);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(109, 42);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Encolar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnEncolar.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncolar.Location = new System.Drawing.Point(328, 16);
+            this.btnEncolar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnEncolar.Name = "btnEncolar";
+            this.btnEncolar.Size = new System.Drawing.Size(100, 45);
+            this.btnEncolar.TabIndex = 0;
+            this.btnEncolar.Text = "Encolar";
+            this.btnEncolar.UseVisualStyleBackColor = true;
+            this.btnEncolar.Click += new System.EventHandler(this.btnEncolar_Click);
             // 
-            // button2
+            // btnDesencolar
             // 
-            this.button2.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(261, 59);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(138, 42);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Desencolar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnDesencolar.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDesencolar.Location = new System.Drawing.Point(307, 69);
+            this.btnDesencolar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnDesencolar.Name = "btnDesencolar";
+            this.btnDesencolar.Size = new System.Drawing.Size(134, 45);
+            this.btnDesencolar.TabIndex = 1;
+            this.btnDesencolar.Text = "Desencolar";
+            this.btnDesencolar.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(50, 138);
+            this.panel1.Location = new System.Drawing.Point(34, 150);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(507, 173);
+            this.panel1.Size = new System.Drawing.Size(591, 247);
             this.panel1.TabIndex = 3;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(147)))), ((int)(((byte)(81)))));
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.numUpDown);
             this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Controls.Add(this.button1);
-            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.btnEncolar);
+            this.panel2.Controls.Add(this.btnDesencolar);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(626, 114);
+            this.panel2.Size = new System.Drawing.Size(657, 142);
             this.panel2.TabIndex = 4;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(579, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(602, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(41, 42);
+            this.pictureBox1.Size = new System.Drawing.Size(42, 36);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
+            // numUpDown
+            // 
+            this.numUpDown.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numUpDown.Location = new System.Drawing.Point(134, 57);
+            this.numUpDown.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.numUpDown.Maximum = new decimal(new int[] {
+            99,
+            0,
+            0,
+            0});
+            this.numUpDown.Name = "numUpDown";
+            this.numUpDown.Size = new System.Drawing.Size(88, 36);
+            this.numUpDown.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(119, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Escoja un valor";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(626, 345);
+            this.ClientSize = new System.Drawing.Size(657, 429);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ejercicio 1 Practico 1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDown)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnEncolar;
+        private System.Windows.Forms.Button btnDesencolar;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numUpDown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -37,5 +37,57 @@ namespace Ejercicio_1_Desafio_Practico_1
         {
 
         }
+
+        private void btnEncolar_Click(object sender, EventArgs e)
+        {
+
+            int valor = Convert.ToInt32(numUpDown.Value);
+            Label miLabel = new Label();
+            miLabel.Text = valor.ToString();
+            miLabel.BackColor = Color.White;
+            miLabel.Height = 50;
+            miLabel.Width = 50;
+            miLabel.TextAlign = ContentAlignment.MiddleCenter;
+            miLabel.BorderStyle = BorderStyle.FixedSingle;
+            miLabel.Location = new Point(0, 0);
+
+            if (colaPrincipal.Contains(miLabel))
+            {
+                MessageBox.Show("El valor ya se encuentra en la Cola, pruebe con otro valor", "Advertencia!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            colaPrincipal.Enqueue(miLabel);
+            
+            /*if (colaPrincipal.Contains(miLabel))
+            {
+                MessageBox.Show("El valor ya se encuentra en la Cola, pruebe con otro valor", "Advertencia!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }*/
+
+            panel1.Controls.Add(miLabel);
+            timer1.Start();
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            btnEncolar.Enabled = false;
+            Label PrimeroCola = colaPrincipal.Peek();
+            PrimeroCola.BackColor = Color.Aqua;
+
+            if (PrimeroCola.Location.X < 450) 
+            {
+                PrimeroCola.Location = new Point(PrimeroCola.Location.X + 5, PrimeroCola.Location.Y);
+                return;
+            }
+            
+            
+
+
+
+
+        }
     }
 }
